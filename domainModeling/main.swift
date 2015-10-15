db869ae1dd0ea07af876921c989154c1cd0e58bd
hmcdonough <hmcd311@gmail.com>
2015-10-15 09:35:13 -0700

    Person completed

--- domainModeling/main.swift
+++ domainModeling/main.swift
@@ -78,8 +78,33 @@ var m2 = Money(a: 10.0, c: Currency.USD)
 var m3 = Money(a: 10.0, c: Currency.GBP)
 print(m1.add(m2))
 print(m1.add(m3)) //should give 25 usd.
-var m4 = Money(a: m1.add(m3), c: m1.currency)
+var m4 = Money(a: m1.add(m3), c: m1.currency) //identical test - 25 USD
 print(m4)
+
+class Job {
+    var title : String
+    var salary: Money
+    var isPerHour : Bool
+    
+    init(t: String, m: Money, b: Bool) {
+        title = t
+        salary = m
+        isPerHour = b
+    }
+//    •calculateIncome, which should accept a number of
+//    hours worked this year
+//    if this is a per-year salary, then ignore the hours
+//    •raise, which will bump up the salary by the passed percentage
+    func calculateIncome(numHours: Int?) -> Double{
+        if !isPerHour {
+            return salary.amount
+        } else if numHours != nil {
+            return salary.amount * Double(numHours!)
+        } else {
+            return -1.0
+        }
+    }
+}
 
 class Person {
     let firstName : String
@@ -88,28 +113,36 @@ class Person {
     var job : Job?
     var spouse : Person?
     
-    init(first: String, last: String, howOld: Int) {
+    init(first: String, last: String, howOld: Int, work: Job?, sp: Person?) {
         firstName = first
         lastName = last
         age = howOld
+        job = nil
+        spouse = nil
+        
+        if howOld > 17 && sp != nil {
+            spouse = sp!
+        }
+        if howOld > 15 && work != nil {
+            job = work!
+        }
     }
     
     func toString() -> String {
         var str = "Name: " + firstName + " " + lastName + " | age: " + String(age)
         if job != nil {
-            str += " | job: " + job!.name
+            str += " | job: " + job!.title
+        } else {
+            str += " | job: None"
         }
         if spouse != nil {
             str += " | spouse: " + spouse!.firstName + " " + spouse!.lastName
+        } else {
+            str += " | spouse: None"
         }
         return str
     }
 }
 
-class Job {
-    let name : String
-    
-    init(jobName: String) {
-        name = jobName
-    }
-}
+var p1 = Person(first: "Paul", last: "Blart", howOld: 40, work: Job(t: "Mall Cop", m: Money(a: 20000, c: Currency.USD ), b: false), sp: nil)
+print(p1.toString())
